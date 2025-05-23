c59e8e2af4f35030298e3642e9477745598d7e20
Manas Jayanth <3097018+ManasJayanth@users.noreply.github.com>
2025-05-23 14:16:30 +0530

    CI

---
+++ build-site.el
@@ -0,0 +1,32 @@
+;; Set the package installation directory so that packages aren't stored in the
+;; ~/.emacs.d/elpa path.
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+;; Initialize the package system
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Install dependencies
+(package-install 'htmlize)
+(package-install 'ox-rss)
+
+;; Load the publishing system
+(require 'ox-publish)
+
+;; Customize the HTML output
+(setq org-html-validation-link nil            ;; Don't show validation link
+      org-html-head-include-scripts nil       ;; Use our own scripts
+      org-html-head-include-default-style nil ;; Use our own styles
+)
+
+;; Define the publishing project
+(load-file "./init.el")
+
+;; Generate the site output
+(org-publish-all t)
+
+(message "Build complete!")
